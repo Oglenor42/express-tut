fce800d69d137a9a5e8445dbc4b723cf609c2e4d
Oglenor42 <ftjudq@inf.elte.hu>
2024-01-31 13:21:11 +0100

    reconnect

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({hi: 'szia'});
+  res.send({hi: 'szia++'});
 });
 
 const PORT = process.env.PORT || 5000;
